49bfe3961d53d72363b35179725812e64b4bd9d6
Martinegeli <martinegeli9@gmail.com>
2020-11-09 12:28:23 +0100

    Major restructuring with using react-app instead

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Velkommen til pilserpils.no</h1>
+      <img src="/dahls.png" alt="Pilserpils" />
+    </div>
+  );
+}
+
+export default App;
