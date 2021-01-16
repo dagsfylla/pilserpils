032c3e568798d859ea4c38f66b1be03317f769ef
Thomas Alsten <thomasb-i@hotmail.com>
2021-01-16 16:56:30 +0100

    Secret message added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-
+import ConfirmButton from './ConfirmButton';
 import StoreCard from "./components/storeCard"
+
 
 function App() {
   return (
     <div className="container">
       <h1>Velkommen til pilserpils.no</h1>
+      <h1>Et nettsted for folk som digger pils</h1>
       <p>En nettbutikk for alt som er relatert til at pils er pils</p>
       <StoreCard />
+      <ConfirmButton 
+      dialog = {['Vil du vite en hemmelighet?', 'sikker?', 'BV er HeadOfBalls!']} 
+      action = {() => console.log('action!')} />
     </div>
   );
 }
---
+++ src/ConfirmButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ConfirmButton extends Component {
+
+static propTypes = {
+    action: PropTypes.func.isRequired
+};
+
+state = {
+    timesPressed: 0
+}
+
+onPress = () => {
+    const { timesPressed } = this.state;
+    const { action } = this.props; 
+
+    this.setState({
+        timesPressed: timesPressed + 1
+    }, () => {
+        if(this.state.timesPressed == 3){
+            action();
+        }
+    })
+}
+
+    render() {
+        const { timesPressed } = this.state;
+        const { dialog } = this.props;
+        return (
+            <button onClick = {this.onPress}> {dialog[timesPressed]} </button>
+        );
+    }
+}
+
+export default ConfirmButton;
